0ec619a61bce5417443dd143737331503474419b
Liemani <parkjh2768@naver.com>
2021-02-23 21:33:10 +0900

    backup

--- ft_printf/mains/main.c
+++ ft_printf/mains/main.c
@@ -5,12 +5,14 @@ int main()
 {
 	int	i;
 
-	i = 10;
+	i = -1;
 	ft_printf("hi \n");
 	ft_printf("i [as %%d] -> [%d] \n", i);
 	printf("i [as %%d] -> [%d] \n", i);
 	ft_printf("i [as %%p] -> [%p] \n", &i);
 	printf("i [as %%p] -> [%p] \n", &i);
+	ft_printf("i [as %%05.*i] -> [%05.*i] \n", i, i);
+	printf("i [as %%05.*i] -> [%05.*i] \n", i, i);
 //	ft_printf(
 
 	return (0);
