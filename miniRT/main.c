9616bcd96811bd46ab24ac6cec2e1877bc5d2c6c
Liemani <parkjh2768@naver.com>
2021-04-29 23:31:18 +0900

    backup

--- miniRT/main.c
+++ miniRT/main.c
@@ -7,22 +7,23 @@
 
 int	main(void)
 {
-	t_vector3	camera_normalized_orientation = {1.0, 1.0, 1.0};
-	vector3_normalize(camera_normalized_orientation);
-	const t_matrix3	*camera_standard_unit_vector = matrix3_standard_unit_vector(&camera_normalized_orientation);
+	t_vector3	normalized_orientation = {0.01, 0.0, -1.0};
+	vector3_normalize(normalized_orientation);
+	const t_matrix3	standard_unit_vector;
+	matrix3_standard_unit_vector(&normalized_orientation, standard_unit_vector);
 
-	int i = 0;
-	while (i < 3)
+	for (int i = 0; i < 3; ++i)
 	{
-		int j = 0;
-		while (j < 3)
-		{
-			printf("%f, ", ((*camera_standard_unit_vector)[i][j]));
-			++j;
-		}
+		for (int j = 0; j < 3; ++j)
+			printf("%.10lf, ", (standard_unit_vector[i][j]));
 		printf("\n");
-		++i;
 	}
+
+	t_vector3 some_vector = {0.0, 0.0, -1.0};
+	vector3_multiply_matrix3(some_vector, standard_unit_vector);
+	for (int i = 0; i < 3; ++i)
+		printf("%.10lf, ", some_vector[i]);
+	printf("\n");
 
 	return (0);
 }
