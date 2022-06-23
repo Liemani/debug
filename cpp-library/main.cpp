d6cbfa44ab7b709076e3aa1456cfb5c42ca33377
Liemani <parkjh2768@gmail.com>
2022-06-23 20:50:51 +0900

    update cpp-library

--- cpp-library/main.cpp
+++ cpp-library/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include "cpp-library.hpp"
+#include "LMI.hpp"
 
 int main(void) {
-    std::string string = "{\"firstName\": \"John\",\"lastName\": \"Smith\",\"isAlive\": true,\"age\": 27,\"address\": {\"streetAddress\": \"21 2nd Street\",\"city\": \"New York\",\"state\": \"NY\",\"postalCode\": \"10021-3100\"},\"phoneNumbers\": [{\"type\": \"home\",\"number\": \"212 555-1234\"},{\"type\": \"office\",\"number\": \"646 555-4567\"}],\"children\": [],\"spouse\": null}";
+    std::string string = "{\"firstName\": \"John\",\"lastName\": \"Smith\",\"isAlive\": true,\"age\": 27,\"address\": {\"streetAddress\": \"21 2nd Street\",\"city\": \"New York\",\"state\": \"NY\",\"postalCode\": \"10021-3100\",},\"phoneNumbers\": [{\"type\": \"home\",\"number\": \"212 555-1234\"},{\"type\": \"office\",\"number\": \"646 555-4567\"}],\"children\": [],\"spouse\": null}";
 
-    JSONDescriber::describe(std::cout, string);
+    LMI::describe(std::cout, string.c_str());
 
     return 0;
 }
