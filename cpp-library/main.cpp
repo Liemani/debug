9268efd1620bb9b234c33ec9eb2dd6188680fed7
Liemani <parkjh2768@naver.com>
2022-06-30 16:04:01 +0900

    test cpp-library

--- cpp-library/main.cpp
+++ cpp-library/main.cpp
@@ -1,39 +1,45 @@
 #include <iostream>
 #include <sstream>
+#include <cmath>
 #include "LMI.hpp"
 
 using std::cout;
 using std::endl;
-using std::string;
 using namespace LMI;
 
 int main(void) {
-    string jsonCString = "{\"firstName\": \"John\",\"lastName\": \"Smith\",\"isAlive\": true,\"age\": 27,\"address\": {\"streetAddress\": \"21 2nd Street\",\"city\": \"New York\",\"state\": \"NY\",\"postalCode\": \"10021-3100\",},\"phoneNumbers\": [{\"type\": \"home\",\"number\": \"212 555-1234\"},{\"type\": \"office\",\"number\": \"646 555-4567\"}],\"children\": [],\"spouse\": null}";
-    describe(cout, jsonCString.c_str());
+    std::string stringToDescribe;
+
+    std::string jsonCString = "{\"firstName\": \"John\",\"lastName\": \"Smith\",\"isAlive\": true,\"age\": 27,\"address\": {\"streetAddress\": \"21 2nd Street\",\"city\": \"New York\",\"state\": \"NY\",\"postalCode\": \"10021-3100\",},\"phoneNumbers\": [{\"type\": \"home\",\"number\": \"212 555-1234\"},{\"type\": \"office\",\"number\": \"646 555-4567\"}],\"children\": [],\"spouse\": null}";
+    JSONDescriber::describe(cout, jsonCString);
     cout << endl;
 
     cout << endl;
     const char charArray[] = { 'a', 'b', 'c' };
     WrapArray<const char> charWrapArray(charArray, 3);
-    std::ostringstream oss;
-    debugDescription(oss, charWrapArray);
-    describe(cout, oss.str());
+    stringToDescribe = debugDescription(charWrapArray);
+    JSONDescriber::describe(cout, stringToDescribe);
     cout << endl;
 
     cout << endl;
     const WrapChar wrapCharArray[] = { 'd', 'e', 'f' };
     WrapArray<const WrapChar> wrapWrapCharArray(wrapCharArray, 3);
-    oss.str(string());
-    debugDescription(oss, wrapWrapCharArray);
-    describe(cout, oss.str());
+    stringToDescribe = debugDescription(wrapWrapCharArray);
+    JSONDescriber::describe(cout, stringToDescribe);
     cout << endl;
 
     cout << endl;
     const char* cStrings[] = { "ab", "bc", "cd" };
     WrapArray<const char*> cStringArray(cStrings, 3);
-    oss.str(string());
-    debugDescription(oss, cStringArray);
-    describe(cout, oss.str());
+    stringToDescribe = debugDescription(cStringArray);
+    JSONDescriber::describe(cout, stringToDescribe);
+    cout << endl;
+
+    cout << endl;
+    const double doubleArray[] = { 1.2, 12.34, HUGE_VAL, 0, NAN };
+    WrapArray<double> wrapDoubleArray(doubleArray, 5);
+    stringToDescribe = debugDescription(wrapDoubleArray);
+    JSONDescriber::describe(cout, stringToDescribe);
     cout << endl;
 
     return 0;
